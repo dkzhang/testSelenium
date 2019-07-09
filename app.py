cb6bebc499d1b4620570eebc44f491fb29c3c794
dkzhang@Office.PC <527656264@qq.com>
2019-07-09 16:48:02 +0800

    modify app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ firefox_capabilities ={
     # "marionette": True,
 }
 browser = webdriver.Remote("http://192.168.100.7:5555/wd/hub", desired_capabilities=firefox_capabilities)
-browser.get("http://www.163.com")
-print(browser.find_element_by_css_selector('a'))
-browser.get_screenshot_as_file(r"./sample.png")
+browser.get('https://book.douban.com/subject_search?search_text= 9787115445353&cat=1001/')
+elem = browser.find_element_by_class_name("title-text")
+print(elem.get_attribute('href'))
+
+# time.sleep(5)
 browser.quit()
